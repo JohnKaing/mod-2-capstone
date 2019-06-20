e7f5af03ca0a16fcd71e74eb38e8e4655905ce86
Joshua Goran <joshuagoran@gmail.com>
2019-06-20 10:01:06 -0400

    committing JDBC basic structure for my two tables

---
+++ java/src/main/java/com/techelevator/reservation/Reservation.java
@@ -0,0 +1,62 @@
+package com.techelevator.reservation;
+
+import java.util.Date;
+
+public class Reservation {
+
+	int reservation_id;
+	int site_id;
+	String name;
+	Date from_date;
+	Date to_date;
+	Date create_date;
+
+	public int getReservation_id() {
+		return reservation_id;
+	}
+
+	public void setReservation_id(int reservation_id) {
+		this.reservation_id = reservation_id;
+	}
+
+	public int getSite_id() {
+		return site_id;
+	}
+
+	public void setSite_id(int site_id) {
+		this.site_id = site_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getFrom_date() {
+		return from_date;
+	}
+
+	public void setFrom_date(Date from_date) {
+		this.from_date = from_date;
+	}
+
+	public Date getTo_date() {
+		return to_date;
+	}
+
+	public void setTo_date(Date to_date) {
+		this.to_date = to_date;
+	}
+
+	public Date getCreate_date() {
+		return create_date;
+	}
+
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+
+}
---
+++ java/src/main/java/com/techelevator/site/JDBCSiteDAO.java
@@ -0,0 +1,31 @@
+package com.techelevator.site;
+
+import java.util.List;
+
+public class JDBCSiteDAO implements SiteDAO {
+
+	@Override
+	public Site createSite(Site newSite) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Site> returnAllSites() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateSite(Site updatedSite) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean deleteSite(Site siteToDelete) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
